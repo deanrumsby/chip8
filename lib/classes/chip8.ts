3656a3849e539f6516fe84b463d707d099baaa20
Dean Rumsby <dean@deanrumsby.com>
2022-06-15 13:37:50 +0100

    change indentation to 2 spaces on typescript files

--- lib/classes/chip8.ts
+++ lib/classes/chip8.ts
@@ -2,220 +2,220 @@ import DecodedInstruction from "../interfaces/decoded-instruction";
 import Display from "./display";
 
 export default class Chip8 {
-    display: Display;
-    memory: DataView;
-    stack: Uint16Array;
-    PC: number;
-    I: number;
-    registers: Array<number>
-    delayTimer: number;
-    soundTimer: number;
+  display: Display;
+  memory: DataView;
+  stack: Uint16Array;
+  PC: number;
+  I: number;
+  registers: Array<number>
+  delayTimer: number;
+  soundTimer: number;
 
-    constructor() {
-        this.display = new Display();
-        this.memory = new DataView(new ArrayBuffer(4096));
-        this.stack = new Uint16Array(16);
-        this.PC = 0x200;
-        this.I = 0x00;
-        this.registers = [
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 
-        ];
-        this.delayTimer = 0x00;
-        this.soundTimer = 0x00;
+  constructor() {
+    this.display = new Display();
+    this.memory = new DataView(new ArrayBuffer(4096));
+    this.stack = new Uint16Array(16);
+    this.PC = 0x200;
+    this.I = 0x00;
+    this.registers = [
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00,
+    ];
+    this.delayTimer = 0x00;
+    this.soundTimer = 0x00;
+  }
+
+  fetch() {
+    const instruction = this.memory.getUint16(this.PC);
+    this.PC += 2;
+    return instruction;
+  }
+
+  decode(instruction: number) {
+    const decodedInstruction: DecodedInstruction = {
+      type: (instruction & 0b1111000000000000) >> 12,
+      nnn: instruction & 0b0000111111111111,
+      nn: instruction & 0b0000000011111111,
+      n: instruction & 0b0000000000001111,
+      x: (instruction & 0b0000111100000000) >> 8,
+      y: (instruction & 0b0000000011110000) >> 4,
     }
+    return decodedInstruction;
+  }
 
-    fetch() {
-        const instruction = this.memory.getUint16(this.PC);
-        this.PC += 2;
-        return instruction;
-    }
-
-    decode(instruction: number) {
-        const decodedInstruction: DecodedInstruction = {
-            type: (instruction & 0b1111000000000000) >> 12,
-            nnn: instruction & 0b0000111111111111,
-            nn: instruction & 0b0000000011111111,
-            n: instruction & 0b0000000000001111,
-            x: (instruction & 0b0000111100000000) >> 8,
-            y: (instruction & 0b0000000011110000) >> 4,
+  execute(instruction: DecodedInstruction) {
+    switch (instruction.type) {
+      case 0x0:
+        switch (instruction.n) {
+          case 0x0:
+            // CLEAR SCREEN
+            break;
+          case 0xE:
+            // RETURN FROM SUBROUTINE
+            break;
         }
-        return decodedInstruction;
-    }
+        break;
 
-    execute(instruction: DecodedInstruction) {
-        switch (instruction.type) {
-            case 0x0:
-                switch (instruction.n) {
-                    case 0x0:
-                        // CLEAR SCREEN
-                        break;
-                    case 0xE:
-                        // RETURN FROM SUBROUTINE
-                        break;
-                }
-                break;
-            
-            case 0x1:
-                // 1NNN: JUMP
-                // Jumps the program counter to instruction NNN
-                this.PC = instruction.nnn;
-                break;
-            
-            case 0x2:
-                // CALL SUBROUTINE AT NNN
-                break;
+      case 0x1:
+        // 1NNN: JUMP
+        // Jumps the program counter to instruction NNN
+        this.PC = instruction.nnn;
+        break;
 
-            case 0x3:
-                // SKIP INSTRUCTION IF VX === NN
-                break;
+      case 0x2:
+        // CALL SUBROUTINE AT NNN
+        break;
 
-            case 0x4:
-                // SKIP INSTRUCTION IF VX !== NN
-                break;
+      case 0x3:
+        // SKIP INSTRUCTION IF VX === NN
+        break;
 
-            case 0x5:
-                // SKIP INSTRUCTION IF VX === VY
-                break;
+      case 0x4:
+        // SKIP INSTRUCTION IF VX !== NN
+        break;
 
-            case 0x6:
-                // 6XNN: SET
-                // Sets VX = NN
-                this.registers[instruction.x] = instruction.nn;
-                break;
+      case 0x5:
+        // SKIP INSTRUCTION IF VX === VY
+        break;
 
-            case 0x7:
-                // 7XNN: ADD
-                // Sets VX += NN
-                // No flag set on overflow
-                this.registers[instruction.x] += instruction.nn;
-                break;
-            
-            case 0x8:
-                switch (instruction.n) {
-                    case 0x0:
-                        // SET VX = VY
-                        break;
-                    
-                    case 0x1:
-                        // VX |= VY
-                        break;
+      case 0x6:
+        // 6XNN: SET
+        // Sets VX = NN
+        this.registers[instruction.x] = instruction.nn;
+        break;
 
-                    case 0x2:
-                        // VX &= VY
-                        break;
-                    
-                    case 0x3:
-                        // VX ^= VY
-                        break;
+      case 0x7:
+        // 7XNN: ADD
+        // Sets VX += NN
+        // No flag set on overflow
+        this.registers[instruction.x] += instruction.nn;
+        break;
 
-                    case 0x4:
-                        // VX += VY (WITH OVERFLOW VF FLAG)
-                        break;
+      case 0x8:
+        switch (instruction.n) {
+          case 0x0:
+            // SET VX = VY
+            break;
 
-                    case 0x5:
-                        // VX -= VY (AFFECTS VF FLAG)
-                        break;
+          case 0x1:
+            // VX |= VY
+            break;
 
-                    case 0x6:
-                        // OPTION ONE: SET VX = VY, then VX >> 1
-                        // (SHIFTED OUT BIT IN VF FLAG)
-                        // OPTION TWO : VX >> 1
-                        // (SHIFTED OUT BIT IN VF FLAG)
-                        break;
+          case 0x2:
+            // VX &= VY
+            break;
 
-                    case 0x7:
-                        // VX = VY - VX (AFFECTS FLAG)
-                        break;
+          case 0x3:
+            // VX ^= VY
+            break;
 
-                    case 0xE:
-                        // OPTION ONE: SET VX = VY, then VX << 1
-                        // (SHIFTED OUT BIT IN VF FLAG)
-                        // OPTION TWO : VX << 1
-                        // (SHIFTED OUT BIT IN VF FLAG)
-                        break;
-                }
-            
-            case 0xA:
-                // ANNN: SET INDEX
-                // Sets I = NNN
-                this.I = instruction.nnn;
-                break;
+          case 0x4:
+            // VX += VY (WITH OVERFLOW VF FLAG)
+            break;
 
-            case 0xB:
-                // OPTION ONE: JUMP TO NNN + V0
-                // OPTION TWO: JUMP TO XNN + VX
-                break;
+          case 0x5:
+            // VX -= VY (AFFECTS VF FLAG)
+            break;
 
-            case 0xC:
-                // GENERATE RANDOM NUMBER 'R', THEN VX = R & NN
-                break;
-                    
-            case 0xD:
-                // DISPLAY - CHECK DOCS
-                break;
+          case 0x6:
+            // OPTION ONE: SET VX = VY, then VX >> 1
+            // (SHIFTED OUT BIT IN VF FLAG)
+            // OPTION TWO : VX >> 1
+            // (SHIFTED OUT BIT IN VF FLAG)
+            break;
 
-            case 0xE:
-                switch (instruction.n) {
-                    case 0x1:
-                        // SKIP INSTRUCTION IF BUTTON WITH VALUE VX NOT 
-                        // CURRENTLY PRESSED DOWN
-                        break;
-                    
-                    case 0xE:
-                        // SKIP INSTRUCTION IF BUTTON WITH VALUE VX IS 
-                        // CURRENTLY PRESSED DOWN
-                        break;                  
-                }
-            
-            case 0xF:
-                switch (instruction.nn) {
-                    case 0x07:
-                        // SET VX TO CURRENT VALUE OF DELAY TIMER
-                        break;
+          case 0x7:
+            // VX = VY - VX (AFFECTS FLAG)
+            break;
 
-                    case 0x0A:
-                        // HALTS PC UNTIL KEY IS PRESSED
-                        break;
+          case 0xE:
+            // OPTION ONE: SET VX = VY, then VX << 1
+            // (SHIFTED OUT BIT IN VF FLAG)
+            // OPTION TWO : VX << 1
+            // (SHIFTED OUT BIT IN VF FLAG)
+            break;
+        }
 
-                    case 0x15:
-                        // SETS DELAY TIMER TO VALUE OF VX
-                        break;
+      case 0xA:
+        // ANNN: SET INDEX
+        // Sets I = NNN
+        this.I = instruction.nnn;
+        break;
 
-                    case 0x18:
-                        // SETS THE SOUND TIMER TO VALUE IN VX
-                        break;
+      case 0xB:
+        // OPTION ONE: JUMP TO NNN + V0
+        // OPTION TWO: JUMP TO XNN + VX
+        break;
 
-                    case 0x1E:
-                        // SET I += VX (AFFECTS OVERFLOW FLAG)
-                        break;
+      case 0xC:
+        // GENERATE RANDOM NUMBER 'R', THEN VX = R & NN
+        break;
 
-                    case 0x29:
-                        // SET I TO THE FONT ADDRESS OF THE HEX
-                        // CHARACTER IN VX
-                        break;
+      case 0xD:
+        // DISPLAY - CHECK DOCS
+        break;
 
-                    case 0x33:
-                        // BINARY CODED DECIMAL CONVERSION -
-                        // SEE DOCS
-                        break;
+      case 0xE:
+        switch (instruction.n) {
+          case 0x1:
+            // SKIP INSTRUCTION IF BUTTON WITH VALUE VX NOT 
+            // CURRENTLY PRESSED DOWN
+            break;
 
-                    case 0x55:
-                        // STORES REGISTERS TO MEMORY - SEE DOCS
-                        break;
+          case 0xE:
+            // SKIP INSTRUCTION IF BUTTON WITH VALUE VX IS 
+            // CURRENTLY PRESSED DOWN
+            break;
+        }
 
-                    case 0x65:
-                        // LOADS REGISTERS FROM MEMORY - SEE DOCS
-                        break;
-                }
+      case 0xF:
+        switch (instruction.nn) {
+          case 0x07:
+            // SET VX TO CURRENT VALUE OF DELAY TIMER
+            break;
+
+          case 0x0A:
+            // HALTS PC UNTIL KEY IS PRESSED
+            break;
+
+          case 0x15:
+            // SETS DELAY TIMER TO VALUE OF VX
+            break;
+
+          case 0x18:
+            // SETS THE SOUND TIMER TO VALUE IN VX
+            break;
+
+          case 0x1E:
+            // SET I += VX (AFFECTS OVERFLOW FLAG)
+            break;
+
+          case 0x29:
+            // SET I TO THE FONT ADDRESS OF THE HEX
+            // CHARACTER IN VX
+            break;
+
+          case 0x33:
+            // BINARY CODED DECIMAL CONVERSION -
+            // SEE DOCS
+            break;
+
+          case 0x55:
+            // STORES REGISTERS TO MEMORY - SEE DOCS
+            break;
+
+          case 0x65:
+            // LOADS REGISTERS FROM MEMORY - SEE DOCS
+            break;
         }
     }
+  }
 
-    step() {
-        const instruction = this.fetch();
-        const decodedInstruction = this.decode(instruction);
-        this.execute(decodedInstruction);
-    }
+  step() {
+    const instruction = this.fetch();
+    const decodedInstruction = this.decode(instruction);
+    this.execute(decodedInstruction);
+  }
 }
--- lib/classes/display.ts
+++ lib/classes/display.ts
@@ -1,29 +1,37 @@
 export default class Display {
-    canvas: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D
-    width: number;
-    height: number;
-    imageData: ImageData;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D
+  width: number;
+  height: number;
+  imageData: ImageData;
 
-    constructor() {
-        this.canvas = document.querySelector('#display') as HTMLCanvasElement;
-        this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.imageData = this.ctx.createImageData(this.width, this.height);
-    }
+  constructor() {
+    this.canvas = document.querySelector('#display') as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.imageData = this.ctx.createImageData(this.width, this.height);
+  }
 
-    // TO-DO: Magic numbers to be made explicit
-    // offset by 4x + (64 * 4)((31 - y)
-    togglePixel(x: number, y: number) {
-        const offset = (4 * x) - (256 * y) + 7936 + 3;
-        const opacity = this.imageData.data[offset];
-        const newOpacity = 255 - opacity;
-        this.imageData.data[offset] = newOpacity;
-        console.log(this.imageData);
-    }
+  // TO-DO: Magic numbers to be made explicit
+  // offset by 4x + (64 * 4)((31 - y)
+  togglePixel(x: number, y: number) {
+    const offset = (4 * x) - (256 * y) + 7936 + 3;
+    const opacity = this.imageData.data[offset];
+    const newOpacity = 255 - opacity;
+    this.imageData.data[offset] = newOpacity;
+  }
 
-    update() {
-        this.ctx.putImageData(this.imageData, 0, 0);
+  toggleByte(x: number, y: number, byte: number) {
+    const byteString = byte.toString(2);
+    for (let i = 0; i < byteString.length; i++) {
+      if (byteString[i] === '1') {
+        this.togglePixel(x + i, y);
+      }
     }
+  }
+
+  update() {
+    this.ctx.putImageData(this.imageData, 0, 0);
+  }
 }
