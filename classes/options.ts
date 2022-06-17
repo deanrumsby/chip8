11064b35b07a0611b4d83fbf65106d5207650f0f
Dean Rumsby <dean@deanrumsby.com>
2022-06-17 16:12:39 +0100

    add more option buttons and begin work on binds/ handlers

--- classes/options.ts
+++ classes/options.ts
@@ -1,12 +1,26 @@
 export default class Options {
   root: HTMLDivElement;
   programSelect: HTMLSelectElement;
+  run: HTMLButtonElement;
+  pause: HTMLButtonElement;
+  step: HTMLButtonElement;
+  reset: HTMLButtonElement;
 
   constructor() {
     this.root = document.querySelector('#options') as HTMLDivElement;
     this.programSelect = this.createProgramSelect();
-
-    this.root.append(this.programSelect);
+    this.run = this.createButton('Run');
+    this.pause = this.createButton('Pause');
+    this.step = this.createButton('Step');
+    this.reset = this.createButton('Reset');
+  
+    this.root.append(
+      this.programSelect,
+      this.run,
+      this.pause,
+      this.step,
+      this.reset
+    );
   }
 
   createProgramSelect() {
@@ -16,5 +30,17 @@ export default class Options {
       select.append(new Option(option, option));
     }
     return select;
+  }
+
+  createButton(text: string) {
+    const button = document.createElement('button');
+    button.innerText = text;
+    return button;
+  }
+
+  bindLoad(handler: Function) {
+    this.programSelect.addEventListener('change', (event) => {
+      handler(event, this.programSelect.value);
+    });
   }
 }
