907d89a09a1ee390806e9c18bdda4fdc8ff4980e
Dean Rumsby <dean@deanrumsby.com>
2023-06-24 23:32:01 +0100

    style: use different icons

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,17 +1,25 @@
 import { useContext } from "react";
-import { FaPlay, FaPause } from "react-icons/fa";
+import {
+  VscDebugStart,
+  VscDebugPause,
+  VscDebugStepOver,
+} from "react-icons/vsc";
 
 import { Chip8Context, type Chip8 } from "../Chip8";
 
 function ControlPanel() {
-  const { setIsRunning } = useContext(Chip8Context) as Chip8;
+  const { chip8, setIsRunning } = useContext(Chip8Context) as Chip8;
 
-  const iconStyle = "text-2xl mx-3 cursor-pointer";
+  const iconStyle = "text-4xl mx-3 cursor-pointer";
 
   return (
     <div className="flex justify-center">
-      <FaPlay className={iconStyle} onClick={() => setIsRunning(true)} />
-      <FaPause className={iconStyle} onClick={() => setIsRunning(false)} />
+      <VscDebugStart className={iconStyle} onClick={() => setIsRunning(true)} />
+      <VscDebugPause
+        className={iconStyle}
+        onClick={() => setIsRunning(false)}
+      />
+      <VscDebugStepOver className={iconStyle} onClick={() => chip8.step()} />
     </div>
   );
 }
