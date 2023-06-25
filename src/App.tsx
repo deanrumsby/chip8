7f5361dc3f1f8615ecfd8241ed13ee744434631a
Dean Rumsby <dean@deanrumsby.com>
2023-06-25 17:51:04 +0100

    style: use flexbox and borders whilst developing

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import ControlPanel from "./components/ControlPanel";
 function App() {
   return (
     <Chip8Provider>
-      <AppBar />
-      <div className="w-1/2">
-        <Display className="border border-black m-2 w-full" />
-        <ControlPanel />
+      <div className="h-screen">
+        <AppBar />
+        <div className="m-2 flex flex-col gap-2 w-1/2">
+          <Display className="border border-black w-full" />
+          <ControlPanel />
+        </div>
       </div>
     </Chip8Provider>
   );
--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -14,7 +14,7 @@ function ControlPanel() {
   const iconStyle = "text-4xl mx-3 cursor-pointer";
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center border border-black p-2">
       <VscDebugStart className={iconStyle} onClick={play} />
       <VscDebugPause className={iconStyle} onClick={pause} />
       <VscDebugStepOver className={iconStyle} onClick={step} />
