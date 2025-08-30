5a312b514ab8d1a34d0321ce04d8f4dac89f6956
Dean Rumsby <dean@deanrumsby.com>
2025-08-30 15:40:00 +0100

    feat: draft loading-a-program.md

--- chip8-js/chip8.js
+++ chip8-js/chip8.js
@@ -1,6 +1,7 @@
 const V_REG_COUNT = 16;
 const STACK_SIZE = 16;
 const MEMORY_SIZE = 4096;
+const PROG_START = 0x200;
 
 class Chip8 {
     pc = 0;
@@ -11,6 +12,18 @@ class Chip8 {
     v = Array(V_REG_COUNT).fill(0);
     stack = Array(STACK_SIZE).fill(0);
     memory = Array(MEMORY_SIZE).fill(0);
+
+    /**
+     * Loads program into memory
+     * @param {Uint8Array} bytes 
+     */
+    load(bytes) {
+        bytes.forEach((byte, index) => {
+            const offset = PROG_START + index;
+            this.memory[offset] = byte;
+        })
+        console.log('memory', this.memory);
+    }
 }
 
 export default Chip8;
--- chip8-js/index.js
+++ chip8-js/index.js
@@ -2,4 +2,13 @@ import Chip8 from './chip8.js';
 
 const chip8 = new Chip8();
 
-console.log('V4', chip8.v[0x4]);
+const filePicker = document.querySelector('#load');
+
+async function handleFileSelection(event) {
+    const file = event.target.files[0];
+    const buffer = await file.arrayBuffer();
+    const bytes = new Uint8Array(buffer);
+    chip8.load(bytes);
+}
+
+filePicker.addEventListener('change', handleFileSelection);
