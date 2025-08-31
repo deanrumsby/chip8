222fbf139bf6b5427cb81dc5f80617d89a3be23b
Dean Rumsby <dean@deanrumsby.com>
2025-08-31 10:55:26 +0100

    feat: use ArrayBuffer as memory

--- chip8-js/chip8.js
+++ chip8-js/chip8.js
@@ -4,25 +4,32 @@ const MEMORY_SIZE = 4096;
 const PROG_START = 0x200;
 
 class Chip8 {
-    pc = 0;
+    pc = PROG_START;
     i = 0;
     sp = 0;
     dt = 0;
     st = 0;
-    v = Array(V_REG_COUNT).fill(0);
-    stack = Array(STACK_SIZE).fill(0);
-    memory = Array(MEMORY_SIZE).fill(0);
+    v = new Array(V_REG_COUNT).fill(0);
+    stack = new Array(STACK_SIZE).fill(0);
+    memory = new ArrayBuffer(MEMORY_SIZE);
+
+    constructor() {
+        this.view = new DataView(this.memory);
+    }
 
     /**
      * Loads program into memory
      * @param {Uint8Array} bytes 
      */
     load(bytes) {
-        bytes.forEach((byte, index) => {
-            const offset = PROG_START + index;
-            this.memory[offset] = byte;
-        })
+        const memory = new Uint8Array(this.memory);
+        memory.set(bytes, PROG_START);
         console.log('memory', this.memory);
+    }
+
+    fetch() {
+        const instruction = this.view.getUint16(this.pc);
+        console.log('instruction', instruction.toString(16));
     }
 }
 
