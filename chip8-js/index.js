8ce8d29d0dcdff20a788ea36ab6a514466d62528
Dean Rumsby <dean@deanrumsby.com>
2025-09-06 13:35:10 +0100

    feat: add step button

--- chip8-js/index.js
+++ chip8-js/index.js
@@ -4,6 +4,7 @@ import { formatHex } from './utils.js';
 const chip8 = new Chip8();
 
 const filePicker = document.querySelector('#load');
+const stepButton = document.querySelector('#step');
 const disassemblyViewer = document.querySelector('#disassembly-viewer');
 const registersViewer = document.querySelector('#registers-viewer');
 
@@ -12,6 +13,15 @@ async function handleFileSelection(event) {
     const buffer = await file.arrayBuffer();
     const bytes = new Uint8Array(buffer);
     chip8.load(bytes);
+    updateUi();
+}
+
+function handleStep() {
+    chip8.step();
+    updateUi();
+}
+
+function updateUi() {
     updateDisassemblyViewer();
     updateRegistersViewer();
 }
@@ -69,3 +79,6 @@ function updateRegistersViewer() {
 }
 
 filePicker.addEventListener('change', handleFileSelection);
+stepButton.addEventListener('click', handleStep);
+
+updateUi();
